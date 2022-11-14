7ee0761e7013b2c612becc83b4a4fb3cb749921f
Dylan Nico Ambrosi <81829336+Dylan-Nico@users.noreply.github.com>
2022-11-14 10:14:18 +0100

    Add files via upload

---
+++ EmotionDetect.pl
@@ -0,0 +1,63 @@
+
+
+helpme :-
+	write("Tell me, what is your problem?"),nl,
+	repeat,
+	read_string(user, "\n", "\r", End, M),
+	split_string(M," "," ",L),
+	saveinto(L),
+	makeans(L),
+	fail.
+
+makeans(L) :- recognize(L,1),
+	write("How long have you felt that way?"),nl,!.
+
+makeans(L) :- recognize(L,2),
+	write("Tell me more about your family."),nl,!.
+
+makeans(L) :- recognize(L,3),
+	write("Do emotions frighten you?"),nl,!.
+
+makeans(L) :- recognize(L,4),
+	write("Please do not use words like that."),nl,!.
+
+makeans(L) :- recognize(L,5),
+	write("Please be more explicit."),nl,!.
+
+makeans(L) :- recognize(L,6),
+	write("That sound significant."),nl,!.
+
+makeans(L) :- recognize(L,7),
+	write("How are you feeling right now?"),nl,!.
+
+makeans(L) :- recognize(L,8),
+	write("Too many mind games."),nl,!.
+
+%makeans(L) :- recognize(L,9),
+	%write("Earlier you mentioned your mother."),nl,!.
+
+makeans(L) :- recognize(L,10),
+	write("Tell me more."),nl,!.
+
+
+recognize(L,1) :- member("feel",L).
+recognize(L,2) :- member("mother",L). %assert(mother).
+recognize(L,3) :- member("love",L).
+recognize(L,4) :- member("hell",L).
+recognize(L,5) :- L = ["yes"] ; L = ["no"].
+recognize(L,6) :- member("sex",L).
+recognize(L,7) :- member("sadness",L); member("anger",L).
+recognize(L,8) :- member("complex",L); member("fixation",L).
+%recognize(_,9) :- mother. % for repeating if mother is in the database
+recognize(_,10). % for Tell me more.
+
+saveinto(L) :-
+	open('/Users/dylan/Desktop/emotions.txt', append, Out),
+	write(Out, L),
+	nl(Out),
+	close(Out).
+
+
+
+
+
